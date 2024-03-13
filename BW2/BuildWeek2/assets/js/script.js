4a5185a451a8b321bf8c1eb0df25591b7308fcb8
felicecesarano <cesarano.fc@gmail.com>
2024-03-14 00:47:30 +0100

    UPDATE

--- BW2/BuildWeek2/assets/js/script.js
+++ BW2/BuildWeek2/assets/js/script.js
@@ -1,11 +1,14 @@
 const player = document.getElementById('player');
 const btnPlay = document.getElementById('btnPlay');
 const volume = document.getElementById('volume');
+const search = document.getElementById('search');
 const documentCard1 = document.getElementById('containerCard1')
 const documentCard2 = document.getElementById('containerCard2')
 const documentCard3 = document.getElementById('containerCard3')
 const documentCard3_2 = document.getElementById('containerCard3_2')
+const documentCard4 = document.getElementById('containerCard4')
 idBraniPreferiti = [];
+let colors = ['#DC148C', '#8400E7', '#27856A', '#0D73EC', '#777777', '#E13300'];
 let cards3 = [
     '426683117',
     '13420001',
@@ -28,7 +31,7 @@ const getFetch = async (category, id) => {
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': 'c776300d22mshd3bea6709348bbfp1dc7c8jsn11b2c60ef719',
+            'X-RapidAPI-Key': 'c81611be3dmshc4dfc559f330731p135b38jsn1f885d01cc8d',
             'X-RapidAPI-Host': 'deezerdevs-deezer.p.rapidapi.com'
         }
     };
@@ -187,6 +190,34 @@ function createCard3_2(albumData, isNone) {
     documentCard3_2.appendChild(outerDiv);
 }
 
+function createCard4(albumData,index) {
+    // Creazione dell'elemento div con le classi specificate
+    const cardDiv = document.createElement('div');
+    cardDiv.classList.add('rounded-3', 'cardGener', 'mb-3');
+    // Assegna un colore di sfondo dalla lista di colori in base all'indice della card
+    cardDiv.style.backgroundColor = colors[index % colors.length];
+
+
+    // Creazione dell'elemento h4 con la classe specificata e il testo "Musica"
+    const h4Element = document.createElement('h4');
+    h4Element.classList.add('mt-2', 'ms-1', 'text-white', 'circular-regular');
+    h4Element.textContent = albumData.artist.name;
+
+    // Creazione dell'elemento img con la classe specificata, l'attributo src e l'attributo alt
+    const imgElement = document.createElement('img');
+    imgElement.classList.add('shadow');
+    imgElement.setAttribute('src', albumData.artist.picture);
+    imgElement.setAttribute('alt', '');
+
+    // Aggiunta degli elementi figlio al div principale
+    cardDiv.appendChild(h4Element);
+    cardDiv.appendChild(imgElement);
+
+    // Aggiunta del div principale al genitore specificato
+    documentCard4.appendChild(cardDiv);
+}
+
+
 async function fillMain() {
     for (let i = 0; i < cards3.length; i++) {
         const albumData = await getFetch('album', cards3[i]);
@@ -217,6 +248,23 @@ function init() {
     fillMain();
 }
 
+
+search.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const main2 = document.getElementById('main2')
+    const search2 = document.getElementById('search2')
+    const mainNoFooter = document.querySelectorAll('#main > :not(#footer)');
+mainNoFooter.forEach(element => {
+    element.style.display = 'none';
+});
+
+for (let i = 0; i < cards3.length; i++) {
+    const albumData = await getFetch('album', cards3[i]);
+    createCard4(albumData,i);
+}
+main2.style.display = 'inline';
+search2.style.display = 'inline';
+})
 
 console.log('Geolier:', getFetch('album', '426683117'));
 console.log('Gue, Marra:', getFetch('album', '13420001'));
